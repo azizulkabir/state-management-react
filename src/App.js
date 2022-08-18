219ac6ee8c7012335ef7286decaee22edb420136
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-08-18 14:05:21 +0600

    conplete usestate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./Component/Counter";
 
 function App() {
   return (
@@ -8,27 +7,7 @@ function App() {
             <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
                 Simple Counter Application
             </h1>
-
-            
-            <div class="max-w-md mx-auto mt-10 space-y-5">
-                <div
-                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-                >
-                    <div class="text-2xl font-semibold">0</div>
-                    <div class="flex space-x-3">
-                        <button
-                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                        >
-                            Increment
-                        </button>
-                        <button
-                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
-                        >
-                            Decrement
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <Counter></Counter>
         </div>
   );
 }
